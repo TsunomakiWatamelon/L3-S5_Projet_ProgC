2d9a6e260c4077ccd94d0f83808d98d743f7f858
Ritieles <83554610+TsunomakiWatamelon@users.noreply.github.com>
2022-12-17 13:23:02 +0100

    added collision module and few collision functions

---
+++ include/Entity.h
@@ -0,0 +1,18 @@
+
+#ifndef __Entity__
+#define __Entity__
+
+#include "MathTools.h"
+
+#define V 1
+
+typedef enum {
+    up, down, left, right
+ } Direction;
+
+typedef struct {
+   double x;
+   double y;
+} Point;
+
+#endif
