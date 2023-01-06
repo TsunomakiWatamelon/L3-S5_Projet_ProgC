00f16f90322964831883135135315b2a350bd67d
Herve Nguyen <herve.nguyen@edu.univ-eiffel.fr>
2023-01-06 19:06:03 +0100

    added files for the Graphics module

---
+++ include/Graphics.h
@@ -0,0 +1,11 @@
+/**
+ * @file Graphics.h
+ * @author Herve Nguyen (herve.nguyen@edu.univ-eiffel.fr)
+ * @brief Header file for the graphics module
+ * @version 0.1
+ * @date 2023-01-06
+ * 
+ * 
+ */
+
+#include <MLV/MLV_all.h>
---
+++ src/Graphics.c
@@ -0,0 +1,12 @@
+/**
+ * @file Graphics.c
+ * @author Herve Nguyen (herve.nguyen@edu.univ-eiffel.fr)
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-06
+ * 
+ * 
+ */
+
+#include "../include/Graphics.h"
+
