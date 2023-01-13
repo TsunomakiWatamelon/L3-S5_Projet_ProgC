fdea4140eda90bf475d819242aaa880015873aad
HERVE NGUYEN <83554610+TsunomakiWatamelon@users.noreply.github.com>
2023-01-13 20:55:21 +0100

    edited documentation

--- doc/doxygen/html/search/all_a.js
+++ doc/doxygen/html/search/all_a.js
@@ -4,7 +4,9 @@ var searchData=
   ['score_2ec_1',['Score.c',['../_score_8c.html',1,'']]],
   ['score_2eh_2',['Score.h',['../_score_8h.html',1,'']]],
   ['setlocation_3',['setLocation',['../_relic_8h.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c'],['../_relic_8c.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c']]],
-  ['speed_5fverstappen_4',['speed_verstappen',['../struct_player.html#a2be588bb70c79ffebc79b0398f5c5660',1,'Player']]],
-  ['square_5',['square',['../structsquare.html',1,'']]],
-  ['square_6',['Square',['../_terrain_8h.html#ab2687f210fc889bf7ec09e81a4a36238',1,'Terrain.h']]]
+  ['sortleaderboardmana_4',['sortLeaderboardMana',['../_score_8h.html#a84c388b69adee7b25c4ee132897fa0b1',1,'sortLeaderboardMana(Leaderboard *leaderboard):&#160;Score.c'],['../_score_8c.html#a84c388b69adee7b25c4ee132897fa0b1',1,'sortLeaderboardMana(Leaderboard *leaderboard):&#160;Score.c']]],
+  ['sortleaderboardtime_5',['sortLeaderboardTime',['../_score_8h.html#a491db76bb06b2c1efff223eb7c05c8cc',1,'sortLeaderboardTime(Leaderboard *leaderboard):&#160;Score.c'],['../_score_8c.html#a491db76bb06b2c1efff223eb7c05c8cc',1,'sortLeaderboardTime(Leaderboard *leaderboard):&#160;Score.c']]],
+  ['speed_5fverstappen_6',['speed_verstappen',['../struct_player.html#a2be588bb70c79ffebc79b0398f5c5660',1,'Player']]],
+  ['square_7',['square',['../structsquare.html',1,'']]],
+  ['square_8',['Square',['../_terrain_8h.html#ab2687f210fc889bf7ec09e81a4a36238',1,'Terrain.h']]]
 ];
--- doc/doxygen/html/search/functions_8.js
+++ doc/doxygen/html/search/functions_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['setlocation_0',['setLocation',['../_relic_8h.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c'],['../_relic_8c.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c']]]
+  ['setlocation_0',['setLocation',['../_relic_8h.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c'],['../_relic_8c.html#afd3570bf071455fb23149701be553f7f',1,'setLocation(Relic *relic, Point location):&#160;Relic.c']]],
+  ['sortleaderboardmana_1',['sortLeaderboardMana',['../_score_8h.html#a84c388b69adee7b25c4ee132897fa0b1',1,'sortLeaderboardMana(Leaderboard *leaderboard):&#160;Score.c'],['../_score_8c.html#a84c388b69adee7b25c4ee132897fa0b1',1,'sortLeaderboardMana(Leaderboard *leaderboard):&#160;Score.c']]],
+  ['sortleaderboardtime_2',['sortLeaderboardTime',['../_score_8h.html#a491db76bb06b2c1efff223eb7c05c8cc',1,'sortLeaderboardTime(Leaderboard *leaderboard):&#160;Score.c'],['../_score_8c.html#a491db76bb06b2c1efff223eb7c05c8cc',1,'sortLeaderboardTime(Leaderboard *leaderboard):&#160;Score.c']]]
 ];
